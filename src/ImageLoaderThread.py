81868e008fac638570067faabe252e5e30605dfd
Krzysztof Kapusta <krz.kapusta@gmail.com>
2017-03-27 20:33:06 +0200

    Three OpenCV methods to calculate blurriness of image

--- src/ImageLoaderThread.py
+++ src/ImageLoaderThread.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python
 
-from PyQt5.QtCore import pyqtSignal, QRunnable, QObject
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtCore import pyqtSignal, QRunnable, QObject, QSize
+from PyQt5.QtGui import QPixmap
 import ImageOperations
+import cv2
 
 class LoaderSignals(QObject):
     pixmapReady = pyqtSignal(QPixmap)
 
 
-#def _loadWithOpenCVToQT(filename):
-#    cvImage = cv2.imread(filename)
-#    return QPixmap.fromImage(ImageOperations.convCvToImage(cvImage))
+def _getImageForProcessing(cvImage):
+    result = cv2.cvtColor(cvImage, cv2.COLOR_BGR2GRAY)
+
+    height, width = result.shape
+    maxSize = (1920, 1080)
+    scale = 1.0/max(width/maxSize[0], height/maxSize[1])
+    result = cv2.resize(result, None, fx=scale, fy=scale, interpolation=cv2.INTER_CUBIC)
+
+    return result
+
+
+def _calcMetrics(filename, cvImage):
+    cvProcess = _getImageForProcessing(cvImage)
+    blurSobel = ImageOperations.blurrinessSobel(cvProcess)
+    blurLapl = ImageOperations.blurrinessLaplace(cvProcess)
+    blurLaplMod = ImageOperations.blurinessLaplaceMod(cvProcess)
+    print(filename + " blurSobel=" + str(blurSobel) + " blurLapl=" + str(blurLapl) + " blurMod=" + str(blurLaplMod))
+
+
+def _loadWithOpenCVToQT(filename):
+    cvImage = cv2.imread(filename)
+    _calcMetrics(filename, cvImage)
+
+    return QPixmap.fromImage(ImageOperations.convCvToImage(cvImage))
 
 
 class ImageLoaderThread(QRunnable):
@@ -20,6 +42,6 @@ class ImageLoaderThread(QRunnable):
         self.fileToLoad = fileName
 
     def run(self):
-        pixmap = ImageOperations.readPixmapFromFile(self.fileToLoad)
+        pixmap = _loadWithOpenCVToQT(self.fileToLoad)
         self.signals.pixmapReady.emit(pixmap)
 
--- src/ImageOperations.py
+++ src/ImageOperations.py
@@ -3,7 +3,8 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QImageReader, QPixmap
 from Exceptions import CannotReadImageException
-
+import numpy as np
+import cv2
 
 def readImageFromFile(fileName):
     reader = QImageReader(fileName)
@@ -26,20 +27,47 @@ def scaleImage(image, size):
 
 
 def convCvToImage(cvImage):
-    import cv2
-
     height, width, byteValue = cvImage.shape
     bytesPerLine = byteValue * width
 
-    cv2.cvtColor(cvImage, cv2.COLOR_BGR2RGB, cvImage)
+    cvImage = cv2.cvtColor(cvImage, cv2.COLOR_BGR2RGB)
     return QImage(cvImage, width, height, bytesPerLine, QImage.Format_RGB888)
 
 
 def convImageToCv(qImage):
-    import numpy as np
-
     swapped = qImage.convertToFormat(QImage.Format_RGB888).rgbSwapped()
     bits = swapped.bits()
     bits.setsize(swapped.byteCount())
 
     return np.array(bits).reshape(swapped.height(), swapped.width(), 3)
+
+
+# Returns 0-1 float. The bigger the more blurry the image is
+# depth of cv2.CV_32F allocates a lot of memory
+def blurrinessSobel(cvImage, depth=-1):
+    sobelNormX = cv2.norm(cv2.Sobel(cvImage, depth, 1, 0))
+    sobelNormY = cv2.norm(cv2.Sobel(cvImage, depth, 0, 1))
+
+    height, width = cvImage.shape[:2]
+    area = height * width
+
+    sumSq = sobelNormX * sobelNormX + sobelNormY * sobelNormY
+    return 1.0/(sumSq / area + 1e-6)
+
+
+# Returns smaller values for blurry images
+def blurrinessLaplace(cvImage, depth=-1):
+    laplVar = np.var(cv2.Laplacian(cvImage, depth))
+    return laplVar
+
+
+# Returns smaller values for blurry images
+# Modified from http://stackoverflow.com/questions/7765810/is-there-a-way-to-detect-if-an-image-is-blurry/7768918#7768918
+def blurinessLaplaceMod(cvImage, depth=-1):
+    kernelX = np.array([-1, 2, -1], dtype=np.float64)
+    kernelY = cv2.getGaussianKernel(3, -1)
+
+    lxAbsMean = cv2.mean(np.abs(cv2.sepFilter2D(cvImage, depth, kernelX, kernelY)))[0]
+    lyAbsMean = cv2.mean(np.abs(cv2.sepFilter2D(cvImage, depth, kernelY, kernelX)))[0]
+
+    return lxAbsMean + lyAbsMean
