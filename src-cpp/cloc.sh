641023bc54c907b7bb54835f7b754c08f8412a43
Krzysztof Kapusta <krz.kapusta@gmail.com>
2017-10-08 19:36:27 +0200

    Ignore comment lines when calculating CLOC

--- src-cpp/cloc.sh
+++ src-cpp/cloc.sh
@@ -4,4 +4,5 @@ cd Phobos
 cat $(ls *.cpp *.h ; find ImageCache ImageProcessing ImportWizard PhotoContainers ProcessWizard Utils Widgets -name '*.cpp' -or -name '*.h' ;) |\
    grep -v '^$' |\
    grep -v '^#include' |\
+   grep -v '^//' |\
    wc -l
