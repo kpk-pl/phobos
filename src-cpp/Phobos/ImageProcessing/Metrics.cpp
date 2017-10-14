83f433072d5906b4b80fc1f8a6584b0fd5c7d9fd
Krzysztof Kapusta <krz.kapusta@gmail.com>
2017-10-14 16:10:34 +0200

    Configurable weights for metric scoring

--- src-cpp/Phobos/ImageProcessing/Metrics.cpp
+++ src-cpp/Phobos/ImageProcessing/Metrics.cpp
@@ -1,19 +1,46 @@
 #include "Metrics.h"
+#include "ConfigExtension.h"
+#include "ConfigPath.h"
 
 namespace phobos { namespace iprocess { namespace metric {
+
+namespace {
+struct Acc
+{
+  operator double() const
+  {
+    if (totalWeight == 0)
+      return 0;
+    return result / totalWeight;
+  }
+
+  void operator()(boost::optional<double> const& val, double const weight)
+  {
+    result += val.value_or(0) * weight;
+    totalWeight += weight;
+  }
+
+private:
+  double result = 0;
+  double totalWeight = 0;
+};
+} // unnamed namespace
 
 boost::optional<double> Metric::score() const
 {
   if (!seriesMetric)
     return boost::none;
-  // TODO: weighted score
-  double result = 0;
-  result += seriesMetric->blur.value_or(0);
-  result += seriesMetric->noise.value_or(0);
-  result += seriesMetric->contrast.value_or(0);
-  result += seriesMetric->sharpness.value_or(0);
-  result += seriesMetric->depthOfField.value_or(0);
-  return result / 5.0;
+
+  config::ConfigPath const weightPath("metricWeights");
+  Acc result;
+
+  result(seriesMetric->blur, config::qualified(weightPath("blur"), 1.0));
+  result(seriesMetric->noise, config::qualified(weightPath("noise"), 1.0));
+  result(seriesMetric->contrast, config::qualified(weightPath("contrast"), 1.0));
+  result(seriesMetric->sharpness, config::qualified(weightPath("sharpness"), 1.0));
+  result(seriesMetric->depthOfField, config::qualified(weightPath("depthOfField"), 1.0));
+
+  return double(result);
 }
 
 }}} // namespace phobos::iprocess::metric
