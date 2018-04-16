fb4433af023b3015e5c9bc7a7dd441d3af57f4d8
Krzysztof <krz.kapusta@gmail.com>
2018-04-16 23:02:03 +0200

    Include additional folder in cloc

--- src-cpp/cloc.sh
+++ src-cpp/cloc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd Phobos
-cat $(ls *.cpp *.h ; find ImageCache ImageProcessing ImportWizard PhotoContainers ProcessWizard Utils Widgets -name '*.cpp' -or -name '*.h' ;) |\
+cat $(ls *.cpp *.h ; find Dialogs ImageCache ImageProcessing ImportWizard PhotoContainers ProcessWizard Utils Widgets -name '*.cpp' -or -name '*.h' ;) |\
    grep -v '^$' |\
    grep -v '^#include' |\
    grep -v '^//' |\
