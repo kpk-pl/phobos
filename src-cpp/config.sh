4717852ce79b315d8e218611d67857b2ea7d80d5
Krzysztof <krz.kapusta@gmail.com>
2018-05-10 20:45:36 +0200

    Update config.sh to use newest Qt available

--- src-cpp/config.sh
+++ src-cpp/config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-CMAKE_BIN=/home/krzysiek/Qt/5.9/gcc_64/bin
-PROJECT_DIR=/home/krzysiek/Workspace/phobos/src-cpp
+CMAKE_BIN=$(find ~/Qt -type d | grep gcc_64/bin | head -1)
+PROJECT_DIR=~/Workspace/phobos/src-cpp
 PROJECT_NAME=Phobos
 RELEASE=debug
